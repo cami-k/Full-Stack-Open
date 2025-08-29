4372b955ba3eee45e861408e8233d08dad2b05d9
Camilla Koskela <camilla.koskela@aalto.fi>
2025-08-29 10:58:04 +0300

    part 3b done

--- osa3/phonebook_backend/index.js
+++ osa3/phonebook_backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 
 const app = express()
 
@@ -12,7 +11,6 @@ morgan.token('body', function getBody (req) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.use(cors())
 
 app.use(express.static('dist'))
 
