bc0238e8538dccdcf2b90bc29fa0cfea27fb5097
Camilla Koskela <camilla.koskela@aalto.fi>
2025-08-29 09:34:33 +0300

    add dist from frontend to backend

---
+++ osa3/phonebook_backend/README.md
@@ -0,0 +1,1 @@
+https://full-stack-open-gnon.onrender.com
--- osa3/phonebook_backend/index.js
+++ osa3/phonebook_backend/index.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
-
 const cors = require('cors')
 
 const app = express()
-
-app.use(cors())
 
 app.use(express.json())
 
@@ -15,6 +12,9 @@ morgan.token('body', function getBody (req) {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+app.use(cors())
+
+app.use(express.static('dist'))
 
 let numbers = [
   {    
